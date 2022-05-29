72c289e3dd8cf3bdf4307cb461513e4e892940eb
igosja <igosja@ukr.net>
2022-05-29 20:29:34 +0300

    env

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as BaseController;
+
+/**
+ * Class AbstractController
+ * @package App\Controller
+ */
+abstract class AbstractController extends BaseController
+{
+}
